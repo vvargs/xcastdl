cb105c39f375dff42d9ec9217844ae3063defafc
rexazier <65490297+rexazier@users.noreply.github.com>
2022-05-18 10:48:49 +0700

    Create fccdl.py

---
+++ fccdl.py
@@ -0,0 +1,25 @@
+import os
+import unix2base62
+
+url = os.getenv("URL")
+audio_only = os.getenv("AUDIO_ONLY")
+
+args = '--threads 8 --wait --poll-interval POLL_INTERVAL 15 --log-level debug --cookies cookies.txt -o "F/%(channel_name)s %(date)s %(title)s.%(ext)s" %s' % (url)
+
+if audio_only == 'true':
+  download_command = 'sudo fc2-live-dl --quality sound %s ' % (args)
+else:
+  download_command = 'sudo fc2-live-dl -x --wait-for-quality-timeout 15 %s ' % (args)
+
+os.system(download_command)
+
+# if audio_only == 'true' and video_only == 'true':
+#   fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -c copy -map_metadata -1 -movflags +faststart "UL/%s.mp4" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output, output)
+# elif audio_only == 'true':
+#   fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output)
+# elif video_only == 'true':
+#   fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -c copy -map_metadata -1 -movflags +faststart "UL/%s.mp4"' % (output, output)
+# else:
+#   fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output)
+
+# os.system(fix_command)
