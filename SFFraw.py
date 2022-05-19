63738761c653a0868a700a3a5f0469de9bc72a0a
rexazier <65490297+rexazier@users.noreply.github.com>
2022-05-19 21:37:07 +0700

    Create SFFraw.py

---
+++ SFFraw.py
@@ -0,0 +1,13 @@
+import os
+import unix2base62
+
+file_name = os.getenv("FILE_NAME")
+m3u8_url = os.getenv("M3U8_URL")
+video_only = os.getenv("VIDEO_ONLY")
+audio_only = os.getenv("AUDIO_ONLY")
+use_ffmpeg = os.getenv("USE_FFMPEG")
+
+output = "%s [%s]" % (file_name, unix2base62.timename())
+# metadata = '-metadata:g encoding_tool="GA.00.00"'
+download_command = 'streamlink --stream-segment-threads 8 --force-progress -o "output/%s.ts" "%s" best' % (output, m3u8_url)
+os.system(download_command)
