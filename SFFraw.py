432e0c08bba94b09fc4d95dadd93dab9687c87d1
rexazier <65490297+rexazier@users.noreply.github.com>
2022-05-29 10:46:02 +0700

    Update SFFraw.py

--- SFFraw.py
+++ SFFraw.py
@@ -9,5 +9,5 @@ use_ffmpeg = os.getenv("USE_FFMPEG")
 
 output = "%s [%s]" % (file_name, unix2base62.timename())
 # metadata = '-metadata:g encoding_tool="GA.00.00"'
-download_command = 'streamlink --retry-open 3 --retry-streams 30 --retry-max 300 --stream-segment-threads 8 --force-progress -o "output/%s.ts" "%s" best' % (output, m3u8_url)
+download_command = 'streamlink --retry-open 3 --retry-streams 30 --retry-max 300 --stream-segment-threads 8 --force-progress -o "DL/%s.ts" "%s" best' % (output, m3u8_url)
 os.system(download_command)
