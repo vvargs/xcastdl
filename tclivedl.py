9a62411536fa3d4682122719a3311336b0267c3c
rexazier <65490297+rexazier@users.noreply.github.com>
2022-06-11 23:34:00 +0700

    Update tclivedl.py

--- tclivedl.py
+++ tclivedl.py
@@ -4,6 +4,7 @@ import unix2base62
 import json
 import base64
 import os
+from pathlib import Path
 
 file_name = os.getenv("FILE_NAME")
 m3u8_url = os.getenv("M3U8_URL")
@@ -11,33 +12,52 @@ video_only = os.getenv("VIDEO_ONLY")
 audio_only = os.getenv("AUDIO_ONLY")
 cookies = os.getenv("TC_COOKIES")
 
+info_json = 'info.json'
 
-b64_site = "dHdpdGNhc3RpbmcudHY="
-tw_site = base64.b64decode(b64_site).decode("ascii")
-tw_url = f"https://{tw_site}/userajax.php?c=islive&u={user_id}"
-
-response = requests.get(url).json()
+if path.is_file():
+  with open(info_json, 'r') as openfile:
+    response = json.load(openfile)
+else:
+  os.system("python tclivedl_check.py")
+  with open(info_json, 'r') as openfile:
+    response = json.load(openfile)
 
 user_id = response['url'].split("/")[1]
 stream_id = response['url'].split("/")[3]
+stream_url = f"https://twitcasting.tv/{user_id}/metastream.m3u8/?mode=source"
+cover_url = f"https://ssl.twitcasting.tv/{user_id}/thumb/{stream_id}"
+user_id_safe = re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "_", user_id.replace("c:",""))
+
 today = date.today()
 time_name = unix2base62.timename()
-user_id_safe = re.sub(r"[/\\?%*:|\"<>\x7F\x00-\x1F]", "_", user_id.replace("c:",""))
 
 output = "%s %s [%s][%s]" % (user_id_safe, stream_id, today, time_name)
 
-args = '-l info --retry-open 3 --retry-streams 30 --retry-max 300 --stream-segment-threads 8 --force-progress --http-cookie "%s" -o "%s.ts" "%s"' % (cookies, output, m3u8_url)
+user_agent = "Mozilla/5.0 (Linux; U; Android 4.4.2; en-us; LGMS323 Build/KOT49I.MS32310c) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/67.0.3396.87 Mobile Safari/537.36"
+origin = "https://twitcasting.tv/"
+referer = "https://twitcasting.tv/"
+ffinit = '-stats -hide_banner -v error -thread_queue_size 8 -user_agent "%s" -headers "Cookies: %s" -headers "origin: %s" -headers "referer: %s"' % (user_agent, cookies, origin, referer)
 
-download_command = 'streamlink %s best' % (args)
-os.system(download_command)
+print("downloading...")
+download_cover = 'ffmpeg %s -i "%s" -c copy "DL/%s.jpg"' % (ffinit, cover_url, output)
+os.system(download_cover)
 
+print("downloading...")
+download_source = 'ffmpeg %s -threads 8 -i "%s" -threads 8 -c copy -threads 8 "DL/%s.ts"' % (ffinit, stream_url, output)
+os.system(download_source)
+
+print("fixing...")
 if audio_only == 'true' and video_only == 'true':
-  fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -c copy -map_metadata -1 -movflags +faststart "UL/%s.mp4" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output, output)
+  fix_command = 'ffmpeg -hide_banner -i "DL/%s.ts" -c copy -map_metadata -1 -movflags +faststart "UL/%s.mp4" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output, output)
 elif audio_only == 'true':
-  fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output)
+  fix_command = 'ffmpeg -hide_banner -i "DL/%s.ts" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output)
 elif video_only == 'true':
-  fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -c copy -map_metadata -1 -movflags +faststart "UL/%s.mp4"' % (output, output)
+  fix_command = 'ffmpeg -hide_banner -i "DL/%s.ts" -c copy -map_metadata -1 -movflags +faststart "UL/%s.mp4"' % (output, output)
 else:
-  fix_command = 'sudo ffmpeg -hide_banner -i "%s.ts" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output)
+  fix_command = 'ffmpeg -hide_banner -i "DL/%s.ts" -vn -c:a copy -map_metadata -1 -movflags +faststart "UL/%s.m4a"' % (output, output)
 
 os.system(fix_command)
+cover_copy = "cp DL/%s.jpg UL/%s.jpg" % (output, output)
+os.system(cover_copy)
+
+
